46440972a94abdeb1f6c2031eaba456fe058c3ad
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-11-12 21:16:24 +0530

    🪲solved

--- landingpage/src/Components/project/Project.jsx
+++ landingpage/src/Components/project/Project.jsx
@@ -1,153 +1,145 @@
 import React from 'react';
 import './project.scss';
 import { FaFacebookF, FaTwitter } from 'react-icons/fa'; 
+import images from "../../assets";
+
+const projects = [
+  { title: 'Online Banking', imgSrc: images.OB },
+  { title: 'Classic Industry', imgSrc: images.OC },
+  { title: 'BoomBap Audio', imgSrc: images.OF },
+  { title: 'Van Moose', imgSrc: images.OG },
+  { title: 'Jay Moments', imgSrc: images.OM },
+  { title: 'Spark Events', imgSrc: images.OP },
+  { title: 'Casual Wear', imgSrc: images.OR },
+  { title: 'Zazoo Apps', imgSrc: images.OT },
+];
 
 const Project = () => (
- <div className="lp_headers_section_padding" id="home">
-  <section>
-    <span class="section-title">PROJECTS</span>
-    <br/>
-          <h2>Projects That We've Proud of</h2>
-          <br/>
-          <div class="category-list-container">
-            <ul>
-              <li>
-                <button className="active">Show All</button>
-              </li>
-              <li>
-                <button>Design</button>
-              </li>
-              <li>
-                <button>Development</button>
-              </li>
-              <li>
-                <button>Marketing</button>
-              </li>
-              <li>
-                <button>SEO</button>
-              </li>
-            </ul>
+  <div className="lp_headers_section_padding" id="home">
+    <section>
+      <span className="section-title">PROJECTS</span>
+      <br />
+      <h2>Projects That We're Proud Of</h2>
+      <br />
+      
+      {/* Categories section */}
+      <div className="category-list-container">
+        <ul>
+          <li>
+            <button className="active">Show All</button>
+          </li>
+          <li>
+            <button>Design</button>
+          </li>
+          <li>
+            <button>Development</button>
+          </li>
+          <li>
+            <button>Marketing</button>
+          </li>
+          <li>
+            <button>SEO</button>
+          </li>
+        </ul>
+      </div>
+      
+      {/* Projects list */}
+      <div className="projectListContainer">
+        {projects.map((project, index) => (
+          <div className="projectItemContainer" key={index}>
+            <img
+              className="projectImage"
+              src={project.imgSrc}
+              alt={project.title}
+            />
+            <div className="projectTitle">{project.title}</div>
           </div>
-          <div class="project-list-container">
-          <div className="project-item-container">
-            <div class="project-item-p-1">
-            <h3>Online Banking</h3>
-            </div> 
+        ))}
+      </div>
+    </section>
+
+    <section className="team">
+      <h3>Our Team of Consultants</h3>
+      <p>
+        We're only as strong and as knowledgeable as our team. 
+        <br />
+        So here are the men and women customers meet goals and grow companies.
+      </p>
+      
+      <div className="team-member-container">
+        <div className="square-container">
+          <div className="profile-picture">
+            <img src="https://xsgames.co/randomusers/assets/avatars/male/2.jpg" alt="John Whitelong" loading="lazy" />
           </div>
-          <div className="project-item-container">
-            <div class="project-item-p-2">
-            <h3>Classic Industry</h3>
-            </div>
+        </div>
+        <p>John Whitelong</p>
+        <h5>General Manager</h5>
+        <div className="social-links">
+          <li className="icon">
+            <FaFacebookF />
+          </li>
+          <li className="icon">
+            <FaTwitter />
+          </li>
+        </div>
+      </div>
+
+      <div className="team-member-container">
+        <div className="square-container">
+          <div className="profile-picture">
+            <img src="https://xsgames.co/randomusers/assets/avatars/female/77.jpg" alt="Veronica Smith" loading="lazy" />
           </div>
-          <div className="project-item-container">
-            <div class="project-item-p-3">
-              <h3>BoomBap Audio</h3>
-            </div>
+        </div>
+        <p>Veronica Smith</p>
+        <h5>Business Developer</h5>
+        <div className="social-links">
+          <li className="icon">
+            <FaFacebookF />
+          </li>
+          <li className="icon">
+            <FaTwitter />
+          </li>
+        </div>
+      </div>
+
+      <div className="team-member-container">
+        <div className="square-container">
+          <div className="profile-picture">
+            <img src="https://xsgames.co/randomusers/assets/avatars/male/76.jpg" alt="Chris Zimerman" loading="lazy" />
           </div>
-          <div className="project-item-container">
-            <div class="project-item-p-4">
-            <h3>Van Moose</h3>
-            </div>
+        </div>
+        <p>Chris Zimerman</p>
+        <h5>Online Marketer</h5>
+        <div className="social-links">
+          <li className="icon">
+            <FaFacebookF />
+          </li>
+          <li className="icon">
+            <FaTwitter />
+          </li>
+        </div>
+      </div>
+
+      <div className="team-member-container">
+        <div className="square-container">
+          <div className="profile-picture">
+            <img src="https://xsgames.co/randomusers/assets/avatars/female/70.jpg" alt="Mary Villalonge" loading="lazy" />
           </div>
-          <div className="project-item-container">
-            <div class="project-item-p-5">
-            <h3>Jay Moments</h3>
-            </div>
-          </div> 
-          <div className="project-item-container">
-            <div class="project-item-p-6">
-            <h3>Spark Events</h3>
-            </div>
-          </div> 
-          <div className="project-item-container">
-            <div class="project-item-p-7">
-            <h3>Casual Wear</h3>
-            </div>
-          </div>
-          <div className="project-item-container">
-            <div class="project-item-p-8">
-            <h3>Zazoo Apps</h3>
-            </div>
-          </div>
-          </div>
-          </section>
-          <section className="team">
-            <h3>Our Team of Consultants</h3>
-            <p>
-              We're only as strong and as knowledge as our team. 
-              <br />
-              So here are the men and women customers meet goals and grow companies
-            </p>
-            <div className="team-member-container">
-            <div className="square-container" >
-            <div className="profile-picture">
-              <img src="https://xsgames.co/randomusers/assets/avatars/male/2.jpg" alt="Member 1" loading="lazy" />
-              </div>
-              </div>
-              <p>John Whitelong</p>
-              <h5>General Manager</h5>
-              <div class="social-links">
-                 <li className="icon">
-                  <FaFacebookF />
-                 </li>
-                 <li className="icon">
-                  <FaTwitter />
-                 </li>
-              </div>
-            </div>
-            <div className="team-member-container">
-            <div className="square-container" >
-            <div className="profile-picture" >
-              <img src="https://xsgames.co/randomusers/assets/avatars/female/77.jpg" alt="Member 2" loading="lazy" />
-              </div>
-            </div>
-              <p>Veronica Smith</p>
-              <h5>Business Developer</h5>
-              <div class="social-links">
-                 <li className="icon">
-                  <FaFacebookF />
-                 </li>
-                 <li className="icon">
-                  <FaTwitter />
-                 </li>
-              </div>
-            </div>
-            <div className="team-member-container">
-            <div className="square-container" >
-            <div className="profile-picture">
-              <img src="https://xsgames.co/randomusers/assets/avatars/male/76.jpg" alt="Member 3" loading="lazy" />
-              </div>
-              </div>
-              <p>Chris Zimerman</p>
-              <h5>Online Marketer</h5>
-              <div class="social-links">
-                 <li className="icon">
-                  <FaFacebookF />
-                 </li>
-                 <li className="icon">
-                  <FaTwitter />
-                 </li>
-              </div>
-            </div>
-            <div className="team-member-container">
-            <div className="square-container">
-            <div className="profile-picture">
-              <img src="https://xsgames.co/randomusers/assets/avatars/female/70.jpg" alt="MEmber 4" loading="lazy" />
-              </div>
-            </div>
-              <p>Mary Villalonge</p>
-              <h5>Community Manager</h5>
-              <div class="social-links">
-                 <li className="icon">
-                  <FaFacebookF />
-                 </li>
-                 <li className="icon">
-                  <FaTwitter />
-                 </li>
-              </div>
-            </div>
-            </section>
+        </div>
+        <p>Mary Villalonge</p>
+        <h5>Community Manager</h5>
+        <div className="social-links">
+          <li className="icon">
+            <FaFacebookF />
+          </li>
+          <li className="icon">
+            <FaTwitter />
+          </li>
+        </div>
+      </div>
+    </section>
   </div>
 );
 
 export default Project;
+
---
+++ landingpage/src/assets/index.js
@@ -0,0 +1,18 @@
+import Book from '../assets/Book.jpg';
+import CC from '../assets/CC.jpeg';
+import Forest from '../assets/Forest.jpg';
+import Girlswing from '../assets/Girlswing.jpg';
+import Glacier from '../assets/Glacier.jpg';
+import Lakeview from '../assets/Lakeview.jpg';
+import Laptop from '../assets/Laptop.jpg';
+import Mountain from '../assets/Mountain.jpg';
+import OB from '../assets/OB.jpg';
+import OC from '../assets/OC.jpg';
+import OF from '../assets/OF.jpg';
+import OG from '../assets/OG.jpg';
+import OM from '../assets/OM.jpg';
+import OP from '../assets/OP.jpg';
+import OR from '../assets/OR.jpg';
+import OT from '../assets/OT.jpg';
+
+export default { Book, CC, Forest, Girlswing, Glacier, Lakeview, Laptop, Mountain, OB, OC, OF, OG, OM, OP, OR, OT };
