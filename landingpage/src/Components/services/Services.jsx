a7adfc72a5669e3020aeecf66a261d44c02bdd8e
Sudhanshu-Ambastha <sudhanshuambastha2020@gmail.com>
2024-11-15 06:32:29 +0530

    ⬆update

--- landingpage/src/Components/services/Services.jsx
+++ landingpage/src/Components/services/Services.jsx
@@ -1,19 +1,95 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './services.scss';
-import { RectangleHorizontal, ChevronLeft, ChevronRight } from 'lucide-react';
+import { Grid3X3,RectangleHorizontal, ChevronLeft, ChevronRight } from 'lucide-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTh, faTableCells } from '@fortawesome/free-solid-svg-icons';
 import testimonialsData from './testimonials.json';
 
 const Services = () => {
   const [testimonialData, setTestimonialData] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(1);
+  const [isGridView, setIsGridView] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [activePoint, setActivePoint] = useState(1);
+  const [enabledStates, setEnabledStates] = useState({
+    1: false,
+    2: false,
+    3: false,
+  }); 
   const scrollContainerRef = useRef(null);
   
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+    setEnabledStates((prevState) => ({
+      ...Object.fromEntries(Object.keys(prevState).map(key => [key, false])),
+      [category]: !prevState[category], 
+    }));
+  };
+
   const handlePointClick = (point) => {
     setActivePoint(point);
   };
+
+  const getCategoryContent = () => {
+    switch (activeCategory) {
+      case 1:
+        return (
+          <div className="skill-list-container">
+          <h3>Business Services For Companies</h3>
+          <p>Aria provides the most innovative and customized business services in the industry. Our Services section shows how flexible we are for all types of budgets.</p>
+            <div className="skill-item">
+              <h6>Business Development 100%</h6>
+              <div className="progress-bar-p-100">
+                <RectangleHorizontal color="#14bf98" />
+              </div>
+            </div>
+            <div className="skill-item">
+              <h6>Opportunity Spotting 76%</h6>
+              <div className="progress-bar-p-76">
+                <RectangleHorizontal color="#14bf98" />
+              </div>
+            </div>
+            <div className="skill-item">
+              <h6>Online Marketing 90%</h6>
+              <div className="progress-bar-p-90">
+                <RectangleHorizontal color="#14bf98" />
+              </div>
+            </div>
+          </div>
+        );
+      case 2:
+        return (
+          <div className="skill-list-container">
+            <p id='Grid'>
+              1. <b>High quality</b> is on top of our list when it comes to the way we deliver the services<br/>
+              2. <b>Maximum impact</b> is what we look for in our actions<br/>
+              3. <b>Quality standards</b> are important but meant to be exceeded<br/>
+              4. <b>We're always looking</b> for industry leaders to help them win their market position<br/>
+              5. <b>Evaluation</b> is a key aspect of this business and important<br/>
+              6. <b>Ethic</b> procedures are always at the base of everything we do
+            </p>
+          </div>
+        );
+      case 3:
+        return (
+          <div className="skill-list-container">
+            <p>
+              <b>We strive to achieve</b> 100% customer satisfaction for both<br/> types of customers: hiring companies and job seekers.<br/>
+              Types of customers <span className='underline'>with huge potential</span><br/><br/>
+              <b>Our goal is to help</b> your company achieve its full potential and establish long term stability for <span className='undeline'>the stakeholders</span><br/><br/>
+              • It's easy to get a quotation, just call our office anytime<br/>
+              • We'll get back to you with an initial estimate soon<br/>
+              • Get ready to see results even after only 30 days<br/>
+              • Ask for a quote and start improving your business<br/>
+              • Just fill out the form and we'll call you right away
+            </p>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   useEffect(() => {
     const extendedData = [...testimonialsData, ...testimonialsData.slice(0, 3)];
     setTestimonialData(extendedData);
@@ -84,7 +160,6 @@ const Services = () => {
       </h2>
 
       <div className="lp_service-list-container">
-        {/* Service Item 1 */}
         <div className="service-item_1">
           <div className="CC" />
           <h4>Off The Ground</h4>
@@ -99,7 +174,6 @@ const Services = () => {
           <button>Details</button>
         </div>
 
-        {/* Service Item 2 */}
         <div className="service-item_2">
           <div className="Book" />
           <h4>Accelerated Growth</h4>
@@ -114,7 +188,6 @@ const Services = () => {
           <button>Details</button>
         </div>
 
-        {/* Service Item 3 */}
         <div className="service-item_3">
           <div className="Laptop" />
           <h4>Market Domination</h4>
@@ -130,7 +203,6 @@ const Services = () => {
         </div>
       </div>
 
-      {/* Propositions Section */}
       <div className="propositions propositions-1">
         <div className="img" />
         <div className="content">
@@ -167,34 +239,37 @@ const Services = () => {
       <div className="propositions propositions-2">
         <div className="content">
           <div className="category-list-container">
-            <div className="category-item active">
-              <h3><span><FontAwesomeIcon icon="fa-solid fa-grid" /></span> Business</h3>
-            </div>
-            <div className="category-item">
-              <h3><span><FontAwesomeIcon icon={faTableCells} /></span> Expertise</h3>
-            </div>
-            <div className="category-item">
-              <h3><span><FontAwesomeIcon icon={faTh} /></span> Quality</h3>
-            </div>
+            {[1, 2, 3].map((category) => (
+              <div
+                key={category}
+                className={`category-item ${activeCategory === category ? 'active' : ''}`}
+                onClick={() => handleCategoryClick(category)}
+              >
+                <h3>
+                  <span
+                    onClick={() => handleCategoryClick(category)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <svg
+                      width="20px"
+                      height="20px"
+                      viewBox="0 0 17 17"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill={activeCategory === category ? '#14bf98' : '#c1cace'}
+                    >
+                      <path d="M0 0h5v5h-5v-5zM6 5h5v-5h-5v5zM12 0v5h5v-5h-5zM0 11h5v-5h-5v5zM6 11h5v-5h-5v5zM12 11h5v-5h-5v5zM0 17h5v-5h-5v5zM6 17h5v-5h-5v5zM12 17h5v-5h-5v5z" />
+                    </svg>
+                  </span>
+                  {category === 1
+                    ? "Business"
+                    : category === 2
+                      ? "Expertise"
+                      : "Quality"}
+                </h3>
+              </div>
+            ))}
           </div>
-          <h3>Business Services For Companies</h3>
-          <p>
-            Aria provides innovative and customized business services. Our <span href="#">Services</span> section showcases our flexibility for all budgets.
-          </p>
-          <div className="skill-list-container">
-            <div className="skill-item">
-              <h6>Business Development 100%</h6>
-              <div className="progress-bar-p-100"><RectangleHorizontal color="#14bf98" /></div>
-            </div>
-            <div className="skill-item">
-              <h6>Opportunity Spotting 76%</h6>
-              <div className="progress-bar-p-76"><RectangleHorizontal color="#14bf98" /></div>
-            </div>
-            <div className="skill-item">
-              <h6>Online Marketing 90%</h6>
-              <div className="progress-bar-p-90"><RectangleHorizontal color="#14bf98" /></div>
-            </div>
-          </div>
+          {getCategoryContent()}
         </div>
         <div className="img" />
       </div>
